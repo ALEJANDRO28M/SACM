bf30fc3cbcc98f7809a8165eb9a0ac4ec3ee4797
alejandro28M7 <laforero64@misena.edu.co>
2024-12-29 02:23:58 -0500

    index principal completado 80% falta todo el tema de las urls para acceder a las bases de datos

--- Frontend/frontendSacm/src/DiseñosReactUser/Sacm.jsx
+++ Frontend/frontendSacm/src/DiseñosReactUser/Sacm.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "../img/indexBackground.webp";
 import "../css/index.css";
-
+import image1 from '../img/salud1.jpg';
+import image2 from '../img/salud2.webp';
+import image3 from '../img/salud3.webp';
+import image4 from '../img/salud4.jpg';
 
 function Prueba() {
   return (
@@ -28,15 +31,28 @@ function Prueba() {
         <div className="cont-menu">
           <nav>
             <a href="#">Portafolio</a>
-            <a href="#">Servicios</a>
-            <a href="#">Suscribirse</a>
-            <a href="#">Facebook</a>
-            <a href="#">Youtube</a>
-            <a href="#">Instagram</a>
+            <a href="#">Bases de Datos</a>
+            <a href="#">Reportes</a>
+            <a href="#">Citas Pendientes</a>
+            <a href="#">Perfil</a>
+            <a href="#">Cerrar Sesion</a>
           </nav>
           <label htmlFor="btn-menu">✖️</label>
         </div>
       </div>
+      <div className="container-body">
+        <h1>LA SALUD DE NUESTROS PACIENTES ES PRIMORDIAL</h1>
+        <br /><br />
+        <h3>
+        La salud es el pilar de la vida, y en SACM estamos aquí para protegerla. Nos comprometemos a brindar soluciones médicas innovadoras, servicios confiables y un acompañamiento humano para garantizar tu bienestar en cada etapa de la vida. Porque cuidar de ti no es solo nuestra misión, es nuestra pasión.
+        </h3>
+      </div>
+      <section className="container/img">
+        <img src={image1} alt="" />
+        <img src={image2}alt="" />
+        <img src={image3}alt="" />
+        <img src={image4} alt="" />
+      </section>
     </body>
   );
 }
