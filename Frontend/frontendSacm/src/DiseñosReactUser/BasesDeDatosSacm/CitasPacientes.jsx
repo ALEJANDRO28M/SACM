fc2306ca3b40305b592ae84791d12038eaf1a3da
alejandro28M7 <laforero64@misena.edu.co>
2025-07-30 10:03:00 -0500

    refactor: Se modifica diseño de Sacm

--- Frontend/frontendSacm/src/DiseñosReactUser/BasesDeDatosSacm/CitasPacientes.jsx
+++ Frontend/frontendSacm/src/DiseñosReactUser/BasesDeDatosSacm/CitasPacientes.jsx
@@ -156,6 +156,9 @@ function CitasPacientes() {
             ))}
           </tbody>
         </table>
+            <button className="btn-volver-pacientes">
+      <a href="http://localhost:5173/BasesSacm.html" className="btn-Tables-Volver">Volver</a>
+    </button>
       </div>
     </div>
   
