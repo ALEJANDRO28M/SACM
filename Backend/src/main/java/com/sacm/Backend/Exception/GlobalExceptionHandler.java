af4b83e9323a94bb0af950acf1eb0c72a012ba60
alejandro28M7 <laforero64@misena.edu.co>
2025-07-30 09:54:47 -0500

    feat: Se crea clase global que manejara la funcionalidad de las excepciones

---
+++ Backend/src/main/java/com/sacm/Backend/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.sacm.Backend.Exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+//INDICA QUE ESTA CLASE SE ENCARGARA DE MANEJAR TODO TIPO DE
+//EXCEPCIONES, COMO TAMBIEN TIENE SIERTAS FUNCIONALIDADES
+//COMO VALIDACIONES, LOGS
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
