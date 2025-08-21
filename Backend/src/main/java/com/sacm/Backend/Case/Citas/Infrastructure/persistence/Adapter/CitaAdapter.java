2d8457e43356c95133df68e7b6457355ffd83ee9
alejandro28M7 <laforero64@misena.edu.co>
2025-08-20 21:32:56 -0500

    refactor: Se Refactoriza el nombre del archivo

---
+++ Backend/src/main/java/com/sacm/Backend/Case/Citas/Infrastructure/persistence/Adapter/CitaAdapter.java
@@ -0,0 +1,61 @@
+package com.sacm.Backend.Case.Citas.Infrastructure.persistence.Adapter;
+
+import com.sacm.Backend.Case.Citas.Application.Port.Out.CitaRepository_OutPort;
+import com.sacm.Backend.Case.Citas.Domain.Models.Citas;
+import com.sacm.Backend.Case.Citas.Infrastructure.Mappers.CitasMapper;
+import com.sacm.Backend.Case.Citas.Infrastructure.persistence.Entities.CitasEntitys;
+import com.sacm.Backend.Case.Citas.Infrastructure.persistence.Repositories.SpringDateCitasRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CitaAdapter implements CitaRepository_OutPort {
+
+
+    private final SpringDateCitasRepository springDateCitasRepository;
+
+    public CitaAdapter(SpringDateCitasRepository springDateCitasRepository) {
+        this.springDateCitasRepository = springDateCitasRepository;
+    }
+
+    @Override
+    public Citas create(Citas citas) {
+        CitasEntitys citasEntity = CitasMapper.toEntity(citas);
+        CitasEntitys savedEntity =  springDateCitasRepository.save(citasEntity);
+        return CitasMapper.toDomain(savedEntity);
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        if (springDateCitasRepository.existsById(id)) {
+            springDateCitasRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Citas update(Citas citas) {
+
+        CitasEntitys savedUpdate = CitasMapper.toEntity(citas);
+        CitasEntitys responseUpdate = springDateCitasRepository.save(savedUpdate);
+        return CitasMapper.toDomain(responseUpdate);
+
+    }
+
+    @Override
+    public Citas findById(Long id) {
+        Citas response = springDateCitasRepository.findById(id)
+                .map(CitasMapper::toDomain)
+                .orElseThrow(() -> new RuntimeException("Citas no encontrada"));
+        return response;
+    }
+
+    @Override
+    public List<Citas> findAll() {
+       final List<CitasEntitys> savedEntity  =  springDateCitasRepository.findAll();
+       List<Citas> response =  CitasMapper.toListCitas(savedEntity);
+       return response;
+    }
+}
