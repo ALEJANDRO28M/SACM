940b3737c19e8558b165f1afff342e5fcdf6d10c
alejandro28M7 <laforero64@misena.edu.co>
2025-08-20 21:34:26 -0500

    feat: Se crea funcionalidad crud y endpoints correspondientes

---
+++ Backend/src/main/java/com/sacm/Backend/Case/Citas/Application/Port/Out/CitaRepository_OutPort.java
@@ -0,0 +1,15 @@
+package com.sacm.Backend.Case.Citas.Application.Port.Out;
+
+import com.sacm.Backend.Case.Citas.Domain.Models.Citas;
+
+import java.util.List;
+
+public interface CitaRepository_OutPort {
+
+    Citas create(Citas citas);
+    Boolean delete(Long id);
+    Citas update(Citas citas);
+    Citas findById(Long id);
+    List<Citas> findAll();
+
+}
