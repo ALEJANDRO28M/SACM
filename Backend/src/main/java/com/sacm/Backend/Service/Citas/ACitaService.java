e7734c0de56b37ee4b46d268d2ed7b2ae3a21db7
alejandro28M7 <laforero64@misena.edu.co>
2025-08-20 21:32:34 -0500

    refactor: Se Refactoriza el nombre del archivo

--- Backend/src/main/java/com/sacm/Backend/Service/Citas/ACitaService.java
+++ Backend/src/main/java/com/sacm/Backend/Service/Citas/ACitaService.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.sacm.Backend.Models.Citas;
 
 @Service
-public class CitaService {
+public class ACitaService {
 
     @Autowired
     CitaDao dao;
