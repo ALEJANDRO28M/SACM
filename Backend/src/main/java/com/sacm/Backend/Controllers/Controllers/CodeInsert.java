1bf810e153f27cf8ed2f61d8242c5647fab522c9
alejandro28M7 <laforero64@misena.edu.co>
2025-09-03 19:51:08 -0500

    refactor: se eliminan clases deprecadas (Que no estan en uso )

--- Backend/src/main/java/com/sacm/Backend/Controllers/Controllers/CodeInsert.java
+++
@@ -1,9 +0,0 @@
-package com.sacm.Backend.Controllers.Controllers;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sacm.Backend.Models.ModelCode;
-
-public interface CodeInsert extends CrudRepository<ModelCode, Integer>{
-
-}
