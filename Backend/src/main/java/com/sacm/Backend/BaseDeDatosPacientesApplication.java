93d1c5dcf4a78781f5596856352d2568096fb49f
alejandro28M7 <laforero64@misena.edu.co>
2025-09-08 17:11:39 -0500

    refactor: Se eliminan conjunto de codigo que no corresponde a ningun tipo de funcionalidad del modulo principal

--- Backend/src/main/java/com/sacm/Backend/BaseDeDatosPacientesApplication.java
+++ Backend/src/main/java/com/sacm/Backend/BaseDeDatosPacientesApplication.java
@@ -6,13 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.sacm.Backend.Controllers.Controllers.RecoverController;
+
 @EnableScheduling
 @SpringBootApplication(exclude= SecurityAutoConfiguration.class)
 public class BaseDeDatosPacientesApplication implements CommandLineRunner{
 
-	@Autowired	
-     RecoverController recoverController;
 	public static void main(String[] args) {
 		SpringApplication.run(BaseDeDatosPacientesApplication.class, args);
 	}
