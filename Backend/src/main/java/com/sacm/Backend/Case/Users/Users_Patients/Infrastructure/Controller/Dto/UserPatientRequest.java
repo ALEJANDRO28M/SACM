af7593320944374fc78813fd67025aeca74d8e18
alejandro28M7 <laforero64@misena.edu.co>
2025-09-11 19:37:07 -0500

    feat: Se crea 2 clases Dto para el manejo de datos de UserPatient

---
+++ Backend/src/main/java/com/sacm/Backend/Case/Users/Users_Patients/Infrastructure/Controller/Dto/UserPatientRequest.java
@@ -0,0 +1,21 @@
+package com.sacm.Backend.Case.Users.Users_Patients.Infrastructure.Controller.Dto;
+
+import com.sacm.Backend.Case.Citas.Infrastructure.persistence.Entities.CitasEntitys;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record UserPatientRequest(
+        Long id,
+        String nombres,
+        String apellidos,
+        String email,
+        String edad,
+        String telefono,
+        String cc,
+        LocalDate fecha_Nacimiento,
+        String genero,
+        /*        HistorialMedico doctor,*/
+        List<CitasEntitys> citas
+) {
+}
