106587d7281e3c9a7e1d668dce8cda737c844c5d
alejandro28M7 <laforero64@misena.edu.co>
2026-01-08 19:09:08 -0500

    feat: Se corrige metodos necesarior, ten en cuenta cualquier tipo de validacion, como tambien tener en cuenta cualquier informacion que nos arroje el sistema

---
+++ Backend/src/main/java/com/sacm/Backend/Case/Login/Infrastructure/persistence/Repositories/SpringDataRole.java
@@ -0,0 +1,13 @@
+package com.sacm.Backend.Case.Login.Infrastructure.persistence.Repositories;
+
+import com.sacm.Backend.Case.Login.Infrastructure.persistence.Entities.RoleEntity;
+import com.sacm.Backend.Case.Login.Infrastructure.persistence.Entities.UserLoginEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SpringDataRole extends JpaRepository<RoleEntity,Long> {
+    Optional<RoleEntity> findByRole(String role);
+}
