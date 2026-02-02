2be3a49570c31f0978681efbe68206fa0fbe6bcc
alejandro28M7 <laforero64@misena.edu.co>
2026-02-02 18:44:24 -0500

    delete: Se eliminan clases deprecadas

--- Backend/src/main/java/com/sacm/Backend/Case/Login/Application/Port/In/LoginUserCaseIn.java
+++
@@ -1,9 +0,0 @@
-package com.sacm.Backend.Case.Login.Application.Port.In;
-
-import com.sacm.Backend.Case.Login.Domain.Models.UserLogin;
-
-import java.util.Map;
-
-public interface LoginUserCaseIn {
-    Map<String, Object> loginUser(UserLogin login);
-}
--- Backend/src/main/java/com/sacm/Backend/Case/Login/Application/Port/In/UpdateUserLoginCase.java
+++
@@ -1,8 +0,0 @@
-package com.sacm.Backend.Case.Login.Application.Port.In;
-
-import com.sacm.Backend.Case.Login.Domain.Models.UserLogin;
-import com.sacm.Backend.Case.Users.Users_Patients.Domain.User;
-
-public interface UpdateUserLoginCase {
-    UserLogin update(UserLogin user);
-}
